cf615a6dbe74c7c52647ee0ec0c7677d404fd40e
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-05-07 19:58:38 +0300

    Updated Ledge Grab System

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    [SerializeField] Vector3 _handPos, _standPos;
+
+    public Vector3 standPos
+    {
+        get
+        {
+            return _standPos;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "GrabCheck")
+        {
+            //Debug.Log("Grab Check");
+            Player player = other.GetComponentInParent<Player>();
+            if(player != null)
+            {
+                player.LedgeGrab(_handPos, this);
+            }
+        }
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jump = 15f;
     [SerializeField] private float _gravity = 1f;
     private bool _jumping = false;
+    private bool _onLedge = false;
+    private Ledge _activeLedge;
 
     private Vector3 _direction, _velocity;
 
@@ -18,7 +20,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         _cc = GetComponent<CharacterController>();
-        if(_cc == null)
+        if (_cc == null)
         {
             Debug.LogError("Player could not fidnd the Character Controller component");
         }
@@ -32,16 +34,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_cc.isGrounded)
+        Move();
+
+        if(_onLedge)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                _animator.SetTrigger("ClimbUp");
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (_cc.isGrounded)
         {
             float horizontalInput = Input.GetAxisRaw("Horizontal");
-            _direction = new Vector3(0, 0, horizontalInput)* _speed;
+            _direction = new Vector3(0, 0, horizontalInput) * _speed;
             _animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
 
-            if(horizontalInput != 0)
+            if (horizontalInput != 0)
             {
                 Vector3 facingSide = transform.localEulerAngles;
-                if(horizontalInput > 0)
+                if (horizontalInput > 0)
                 {
                     facingSide.y = 0;
                 }
@@ -53,7 +68,7 @@ public class Player : MonoBehaviour
                 transform.localEulerAngles = facingSide;
             }
 
-            if(_jumping)
+            if (_jumping)
             {
                 _jumping = false;
                 _animator.SetBool("Jump", false);
@@ -67,6 +82,28 @@ public class Player : MonoBehaviour
             }
         }
         _direction.y -= _gravity;
-        _cc.Move(_direction * Time.deltaTime);        
+        _cc.Move(_direction * Time.deltaTime);
+    }
+
+    public void LedgeGrab(Vector3 handposition, Ledge currentLedge)
+    {
+        _cc.enabled = false;
+        _animator.SetBool("LedgeGrab", true);
+
+        //we do not want to trigger any other animation while hanging on a ledge
+        _animator.SetFloat("Speed", 0.0f);
+        _animator.SetBool("Jump", false);
+        _onLedge = true;
+
+        //this is for a better look 
+        transform.position = handposition;
+        _activeLedge = currentLedge;
+    }
+
+    public void ClimbPosition()
+    {
+        transform.position = _activeLedge.standPos;
+        _animator.SetBool("LedgeGrab", false);
+        _cc.enabled = true;
     }
 }
