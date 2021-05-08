8dd916c8a0ba1d2c5284161a64f8a74b7375d664
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-05-09 00:09:26 +0300

    Elevator Lift Completed

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Elevator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    private bool _goingUp = false;
+    private float _speed = 3.0f;
+    Vector3 _topFloor, _downFloor;
+
+    [SerializeField] private Transform _topTransform, _downTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _topFloor = new Vector3(0, 33.41f, 0);
+        _downFloor = Vector3.zero;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_goingUp)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _topTransform.position, _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _downTransform.position, _speed * Time.deltaTime);
+        }
+
+        if(transform.position == _topTransform.position)
+        {
+            StartCoroutine(ElevatorRoutine());
+            _goingUp = false;
+        }
+        else if(transform.position == _downTransform.position)
+        {
+            StartCoroutine(ElevatorRoutine());
+            _goingUp = true;
+        }
+        
+    }
+
+    IEnumerator ElevatorRoutine()
+    {
+        _speed = 0;
+        yield return new WaitForSeconds(5f);
+        _speed = 3f;
+    }
+
+
+
+}
